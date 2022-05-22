2e9b4f639661b0028bb7c952749e3a39e2080557
LiuYuJSCPPY <uioplret57423@gmail.com>
2022-05-22 18:04:58 +0800

    RoomTypefinish

--- HotelManagementSystem/HotelManagementSystem.Web/Areas/Dashborad/Controllers/RoomTypesController.cs
+++ HotelManagementSystem/HotelManagementSystem.Web/Areas/Dashborad/Controllers/RoomTypesController.cs
@@ -89,59 +89,6 @@ namespace HotelManagementSystem.Web.Areas.Dashborad.Controllers
             return json;
         }
 
-        // GET: Dashborad/RoomTypes/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Dashborad/RoomTypes/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,RoomType")] RoomTypes roomTypes)
-        {
-            if (ModelState.IsValid)
-            {
-                db.roomTypes.Add(roomTypes);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(roomTypes);
-        }
-
-        // GET: Dashborad/RoomTypes/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            RoomTypes roomTypes = db.roomTypes.Find(id);
-            if (roomTypes == null)
-            {
-                return HttpNotFound();
-            }
-            return View(roomTypes);
-        }
-
-        // POST: Dashborad/RoomTypes/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,RoomType")] RoomTypes roomTypes)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(roomTypes).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(roomTypes);
-        }
 
         // GET: Dashborad/RoomTypes/Delete/5
         public ActionResult Delete(int? id)
@@ -155,18 +102,32 @@ namespace HotelManagementSystem.Web.Areas.Dashborad.Controllers
             {
                 return HttpNotFound();
             }
-            return View(roomTypes);
+            return PartialView( "_Delete",roomTypes);
         }
 
         // POST: Dashborad/RoomTypes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        [HttpPost]
+        public JsonResult Delete(RoomTypes roomTypes)
         {
-            RoomTypes roomTypes = db.roomTypes.Find(id);
-            db.roomTypes.Remove(roomTypes);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            RoomTypes DeleteroomTypes = db.roomTypes.Find(roomTypes.Id);
+            JsonResult json = new JsonResult();
+            bool Result = false;
+
+            db.roomTypes.Remove(DeleteroomTypes);
+            Result = db.SaveChanges() > 0;
+
+            if (Result)
+            {
+                json.Data = new { Success = true };
+
+            }
+            else
+            {
+                json.Data = new { Success = false, Message = "刪除失敗" };
+            }
+
+
+            return json;
         }
 
         protected override void Dispose(bool disposing)
