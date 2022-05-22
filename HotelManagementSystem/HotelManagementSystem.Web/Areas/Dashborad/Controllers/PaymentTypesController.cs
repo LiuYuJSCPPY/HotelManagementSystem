69723779023c02f427ceb0d6e6b2fc8792d0ead8
LiuYuJSCPPY <uioplret57423@gmail.com>
2022-05-22 23:37:21 +0800

    PaymentTypes,Controller 原來的CRUD還沒有修改

---
+++ HotelManagementSystem/HotelManagementSystem.Web/Areas/Dashborad/Controllers/PaymentTypesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HotelManagementSystem.Data;
+using HotelManagementSystem.Model;
+
+namespace HotelManagementSystem.Web.Areas.Dashborad.Controllers
+{
+    public class PaymentTypesController : Controller
+    {
+        private HotelManagementSystemContext db = new HotelManagementSystemContext();
+
+        // GET: Dashborad/PaymentTypes
+        public ActionResult Index()
+        {
+            return View(db.paymentTypes.ToList());
+        }
+
+
+
+        public ActionResult Action()
+        {
+
+        }
+        // GET: Dashborad/PaymentTypes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PaymentType paymentType = db.paymentTypes.Find(id);
+            if (paymentType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paymentType);
+        }
+
+        // GET: Dashborad/PaymentTypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Dashborad/PaymentTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,PaymentTypeName")] PaymentType paymentType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.paymentTypes.Add(paymentType);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(paymentType);
+        }
+
+        // GET: Dashborad/PaymentTypes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PaymentType paymentType = db.paymentTypes.Find(id);
+            if (paymentType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paymentType);
+        }
+
+        // POST: Dashborad/PaymentTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,PaymentTypeName")] PaymentType paymentType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(paymentType).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(paymentType);
+        }
+
+        // GET: Dashborad/PaymentTypes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PaymentType paymentType = db.paymentTypes.Find(id);
+            if (paymentType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paymentType);
+        }
+
+        // POST: Dashborad/PaymentTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PaymentType paymentType = db.paymentTypes.Find(id);
+            db.paymentTypes.Remove(paymentType);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
