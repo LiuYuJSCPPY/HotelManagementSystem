2d90bddf1cfa98bb23962d592751372564401af1
LiuYuJSCPPY <uioplret57423@gmail.com>
2022-05-22 10:35:51 +0800

    新增了Model

---
+++ HotelManagementSystem/HotelManagementSystem.Data/HotelManagementSystemContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace HotelManagementSystem.Data
+{
+    public class HotelManagementSystemContext : DbContext
+    {
+        public HotelManagementSystemContext():base("name = HotelManagementSystemContext") {
+        
+        }
+    }
+}
